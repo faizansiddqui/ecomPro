2ac06a944c3bcf255ea0372af59fcbc3f336a7fb
faizansiddqui <153535671+faizansiddqui@users.noreply.github.com>
2024-07-10 17:20:23 +0530

    Create Dexcription.jsx

---
+++ Dexcription.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './DiscriptionBox.css'
+
+
+const DescriptionBox = ()=>{
+    return(
+        <div className="descriptionBox">
+            <div className="descriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Review (122) </div>
+            </div>
+            <div className="descriptionBox-description">
+                <p> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                     Possimus laborum sapiente cumque, esse iure accusamus enim
+                    corrupti quo consequatur ipsum iste laboriosam. Quibusdam 
+                    odit accusantium at, dolore rerum assumenda facilis aliquam!
+                    Temporibus rem vel optio molestiae dolor, cupiditate commodi
+                    a voluptatibus provident, amet eligendi doloribus aperiam
+                    quisquam ab libero doloremque!</p>
+                    <p>
+                        Lorem ipsum dolor, sit amet consectetur adipisicing
+                         elit. Earum, voluptates aliquid. Libero consectetur
+                          doloribus amet placeat quaerat, magni enim! Facilis.
+                    </p>
+            </div>
+        </div>
+    )
+}
+export default DescriptionBox;
