bd455eb1645e594676a6f7cc82ececb670aa5227
faizansiddqui <153535671+faizansiddqui@users.noreply.github.com>
2024-07-10 17:09:18 +0530

    Create sowProduct.jsx

---
+++ sowProduct.jsx
@@ -0,0 +1,36 @@
+.showProduct{
+    width: 90vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    margin: auto;
+    padding: 0px 20px 20px 20px;
+    margin-bottom: 50px;
+    background-color: rgb(255, 255, 255);
+    box-shadow: 0px 0px 6px -1px rgb(225, 225, 225);
+}
+.showProduct h1{
+    margin-top: 15px;
+    font-weight: 500;
+    color: rgb(83, 83, 83);
+}
+.rightArrow{
+    font-size: 30px;
+    font-weight: 400;
+    margin-top: 15px;
+    cursor: pointer;
+    color: rgb(39, 72, 255);
+}
+.item-data{
+   display: grid;
+   grid-template-columns: repeat(4, 1fr);
+   grid-template-rows: repeat(1, 1fr);
+   gap: 20px;
+   margin-top: 20px;
+}
+.showProductTop{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
