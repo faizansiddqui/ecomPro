2b0283529bc6d96886b7a7bca8f03b1a749b715a
faizansiddqui <153535671+faizansiddqui@users.noreply.github.com>
2024-07-10 17:06:20 +0530

    Create Cart.jsx

---
+++ Cart.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CartItem from "../Component/CartItems/CartItems";
+
+const Cart = ()=>{
+    return(
+        <div>
+            <CartItem/>
+        </div>
+    )
+}
+
+export default Cart;
